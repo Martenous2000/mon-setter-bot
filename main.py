dc35961563133c27739fcc6e5023bce51e0e7a39
Martenous2000 <martenous2000@gmail.com>
2026-09-03 12:14:00 +0200

    chore: force redeploy Railway pour recharger secteur manufacture Franck

--- main.py
+++ main.py
@@ -1215,6 +1215,8 @@ async def relance(req: RelanceRequest):
 # redeploy trigger 7: suspend l'envoi d'icebreaker sur le compte florian
 # redeploy trigger 8: renforce filtre geo US/Canada uniquement pour Enzo (connexions et icebreakers)
 # redeploy trigger 9: force le rechargement pour nouveau persona sarah-amouyal
+# redeploy trigger 10: ajoute secteur manufacture (usinage, chaudronnerie, electricite industrielle, etc.) pour Franck
+
 
 
 
